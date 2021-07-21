5a7df5f329dd9579011c19df5d05129e534e4253
triartainyoman <triarta.art@gmail.com>
2021-07-21 08:28:59 +0800

    The ListView Builder

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter_complete/models/task.dart';
 import 'package:todoey_flutter_complete/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: "Buy Milk"),
+    Task(name: "Buy Coffee"),
+    Task(name: "Buy Tea"),
+    Task(name: "Buy Book"),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        return TaskTile(
+          taskTitle: tasks[index].name,
+          isChecked: tasks[index].isDone,
+          checkboxCallback: (checkboxState) {
+            setState(() {
+              tasks[index].toggleDone();
+            });
+          },
+        );
+      },
     );
   }
 }
